d8089b9271914c20558bc0a163a7bb8eb96fee4d
EmmanuelOsaea <155497725+EmmanuelOsaea@users.noreply.github.com>
2025-11-02 19:00:54 +0100

    Update Main_Activity.kt

--- Main_Activity.kt
+++ Main_Activity.kt
@@ -4,6 +4,9 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.musicplayer.databinding.ActivityMainBinding
+import androidx.activity.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+ import com.example.musicplayer.ui.MusicViewModel
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -27,5 +30,30 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, MusicService::class.java).setAction("STOP")
             startService(intent)
         }
+    }
+}
+private val viewModel: MusicViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val adapter = TrackAdapter()
+        binding.recyclerTracks.layoutManager = LinearLayoutManager(this)
+        binding.recyclerTracks.adapter = adapter
+
+        viewModel.tracks.observe(this) {
+            adapter.submitList(it)
+        }
+
+        binding.btnDownload.setOnClickListener {
+            val url = binding.inputUrl.text.toString()
+            val title = binding.inputTitle.text.toString()
+            val artist = binding.inputArtist.text.toString()
+            viewModel.downloadTrack(url, title, artist)
+        }
+
+        viewModel.loadTracks()
     }
 }
