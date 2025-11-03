50942dc58155433b839ec642e88b219022d31b97
EmmanuelOsaea <155497725+EmmanuelOsaea@users.noreply.github.com>
2025-11-03 12:34:32 +0100

    Create PlayerActivity.kt

---
+++ PlayerActivity.kt
@@ -0,0 +1,39 @@
+package com.example.musicplayer
+
+import android.media.MediaPlayer
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.musicplayer.databinding.ActivityPlayerBinding
+
+class PlayerActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityPlayerBinding
+    private var mediaPlayer: MediaPlayer? = null
+    private lateinit var currentMusic: MusicModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityPlayerBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        currentMusic = intent.getParcelableExtra("music")!!
+        binding.musicTitle.text = currentMusic.title
+        binding.musicArtist.text = currentMusic.artist
+
+        mediaPlayer = MediaPlayer.create(this, currentMusic.fileRes)
+
+        binding.playPauseBtn.setOnClickListener {
+            if (mediaPlayer!!.isPlaying) {
+                mediaPlayer!!.pause()
+                binding.playPauseBtn.text = "Play"
+            } else {
+                mediaPlayer!!.start()
+                binding.playPauseBtn.text = "Pause"
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mediaPlayer?.release()
+    }
+}
