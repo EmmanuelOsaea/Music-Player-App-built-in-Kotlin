38f14b469bb4cfd3c4a97a368eb95b8f59214e83
EmmanuelOsaea <155497725+EmmanuelOsaea@users.noreply.github.com>
2025-11-03 13:40:33 +0100

    Update Song.kt

--- Song.kt
+++ Song.kt
@@ -1,8 +1,17 @@
 package com.example.musicplayer.model
+package com.example.musicplayer.model
 
 data class Song(
     val title: String,
     val artist: String,
     val path: String,
     val duration: Long
+)
+
+data class Song(
+    val title: String,
+    val artist: String,
+    val path: String,
+    val duration: Long,
+    val albumArtUri: String? = null
 )
